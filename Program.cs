29a8ac5ae7bf36878a4d4a62c033429b1f4942d8
ChobbyCode <jacobhadfield99@gmail.com>
2023-09-03 17:15:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Update_Checker;
+
+namespace UCTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Updater();
+            Console.ReadLine();
+        }
+
+        public static async void Updater()
+        {
+            UpdateChecker updateChecker = new UpdateChecker();
+
+            updateChecker.OWNERNAME = "ChobbyCode";
+            updateChecker.REPONAME = "Update-Checker";
+
+            string currentVersion = "v0.1.0";
+
+            bool isUpdate = await updateChecker.CheckForUpdates(currentVersion);
+
+            if(isUpdate)
+            {
+                // Do update stuff here
+                Console.WriteLine("There is an update");
+            }
+        }
+    }
+}
